c017b4b24d360dadcf5513698369aa5487505297
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-01-23 14:19:08 +0000

    Initial Commit

---
+++ CarCustomiserSwift/CarCustomiserSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserSwiftApp.swift
+//  CarCustomiserSwift
+//
+//  Created by Ouahchia, Mohamed (JD) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCustomiserSwift/Models/Car.swift
@@ -0,0 +1,8 @@
+//
+//  Car.swift
+//  CarCustomiserSwift
+//
+//  Created by Ouahchia, Mohamed (JD) on 23/01/2024.
+//
+
+import Foundation
